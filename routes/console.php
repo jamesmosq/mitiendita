e707b607ad7b4c9f73f11a56ca1a5296a9da3f7f
James Mosquera <Jamesmosqr@gmail.com>
2026-06-07 11:51:37 -0500

    prueba fecha de cron jobs

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('backup:clean')->daily()->at('01:00');
-Schedule::command('backup:run')->daily()->at('02:00');
+Schedule::command('backup:clean')->daily()->at('11:55');
+Schedule::command('backup:run')->daily()->at('12:00');
